e7435626c62de32bfe6381a2d2482444258d2eaa
Arkos Vähämäki <arkos.vahamaki@tuni.fi>
2023-02-01 14:50:25 +0200

    authorization for profile

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,8 +1,32 @@
 import axios from 'axios';
-import React from 'react';
+import React, { Component } from "react";
+import { Navigate } from "react-router-dom";
+import AuthService from "../services/auth.service";
 
-const Profile = props => {
-    
+export default class Profile extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        redirect: null,
+        userReady: false,
+        currentUser: { username: "" }
+      };
+    }
+  
+    componentDidMount() {
+      const currentUser = AuthService.getCurrentUser();
+  
+      if (!currentUser) this.setState({ redirect: "/" });
+      this.setState({ currentUser: currentUser, userReady: true })
+    }
+  
+    render() {
+      if (this.state.redirect) {
+        return <Navigate to={this.state.redirect} />
+      }
+  
+      const { currentUser } = this.state;
     return (
         <>
             <div className="main-content">
@@ -11,6 +35,4 @@ const Profile = props => {
         </>
 
     )
-}
-
-export default Profile;
+}}
