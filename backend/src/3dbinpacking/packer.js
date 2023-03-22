41905f2003206c0585e45d8b55515301fb7c241d
Arkos Vähämäki <arkos.vahamaki@tuni.fi>
2023-03-22 11:04:30 +0200

    package size and weight

--- backend/src/3dbinpacking/packer.js
+++ backend/src/3dbinpacking/packer.js
@@ -4,10 +4,19 @@ const samchon = require("samchon-framework");
 exports.binPacker = (currentOrders, newOrder) => {
     const wrapperArray = new packer.WrapperArray();
     const instanceArray = new packer.InstanceArray();
+    let weight = 0;
 
     wrapperArray.push(
-        new packer.Wrapper("Intl", 24700,50,30,20,0)
+        new packer.Wrapper("Intl", 6300,20,20,20,0)
     );
+
+    for (let i = 0; i < currentOrders.length; i++) {
+        weight = weight + currentOrders[i].product_weight;
+        if (weight > 2) {
+            console.log("Not enough space");
+            return(false);
+        }
+    }
     
     for (let i = 0; i < currentOrders.length; i++) {
         instanceArray.insert(instanceArray.end(), 1, new packer.Product("Product", currentOrders[i].product_x, currentOrders[i].product_y, currentOrders[i].product_z));
--- frontend/src/pages/Table.js
+++ frontend/src/pages/Table.js
@@ -58,9 +58,9 @@ const Table = props => {
             Header: 'International shipping cost (eur)',
             Cell: (original) => (
                 !original.row.original.grouporder_id ? (
-                    (24700*0.0071) + 10
+                    (6300*0.0071) + 10
                 ) : (
-                    (24700*0.0071)/Number(original.row.original.grouporder_members) + 10
+                    (6300*0.0071)/Number(original.row.original.grouporder_members) + 10
                 )
             )
           },
@@ -68,9 +68,9 @@ const Table = props => {
             Header: 'Total price (eur)',
             Cell: (original) => (
                 !original.row.original.grouporder_id ? (
-                    (24700*0.0071) + 10 + (Number(original.row.original.product_price) * 0.0071) + (Number(original.row.original.shipping_cost) * 0.0071) + (Number(original.row.original.product_price) * 0.0071 * 0.04)
+                    (6300*0.0071) + 10 + (Number(original.row.original.product_price) * 0.0071) + (Number(original.row.original.shipping_cost) * 0.0071) + (Number(original.row.original.product_price) * 0.0071 * 0.04)
                 ) : (
-                    (24700*0.0071)/Number(original.row.original.grouporder_members) + 10 + (Number(original.row.original.product_price) * 0.0071) + (Number(original.row.original.shipping_cost) * 0.0071) + (Number(original.row.original.product_price) * 0.0071 * 0.04)
+                    (6300*0.0071)/Number(original.row.original.grouporder_members) + 10 + (Number(original.row.original.product_price) * 0.0071) + (Number(original.row.original.shipping_cost) * 0.0071) + (Number(original.row.original.product_price) * 0.0071 * 0.04)
                 )
             ) 
           }
